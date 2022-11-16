665d5a2ba6281ec6f4d00b563227de2a84f9b4d1
Scott <hscottvo@gmail.com>
2022-11-15 17:24:52 -0800

    Added owmpy Example

--- scratch/test.py
+++ scratch/test.py
@@ -1,8 +1,23 @@
-import requests
-import pandas as pd
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
-# response = requests.get("https://api.weather.gov")
-# print(response)
+owm = OWM("fdcc86acf4e1025e275556082759b630")
+mgr = owm.weather_manager()
 
-df = pd.read_csv("../data/worldcities.csv")
-print(df)
+
+# Search for current weather in London (Great Britain) and get details
+observation = mgr.weather_at_place("London,GB")
+w = observation.weather
+
+w.detailed_status  # 'clouds'
+w.wind()  # {'speed': 4.6, 'deg': 330}
+w.humidity  # 87
+w.temperature("celsius")  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+w.rain  # {}
+w.heat_index  # None
+w.clouds  # 75
+
+# Will it be clear tomorrow at this time in Milan (Italy) ?
+forecast = mgr.forecast_at_place("Milan,IT", "daily")
+answer = forecast.will_be_clear_at(timestamps.tomorrow())
